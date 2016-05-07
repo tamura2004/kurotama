63125f6583dbcc9932cc90ea842954e5cb58c339
tamura <tamura2004@gmail.com>
2016-05-07 17:18:57 +0900

    Fix migration dependency

--- db/migrate/20160508054454_create_moves.rb
+++ db/migrate/20160508054454_create_moves.rb
@@ -4,8 +4,8 @@ class CreateMoves < ActiveRecord::Migration
       t.string :name
       t.references :pc, index: true, foreign_key: true
       t.references :mob, index: true, foreign_key: true
-      t.references :boss, index: true, foreign_key: true
-      t.references :item, index: true, foreign_key: true
+      # t.references :boss, index: true, foreign_key: true
+      # t.references :item, index: true, foreign_key: true
       t.references :map, index: true, foreign_key: true
 
       t.timestamps null: false
